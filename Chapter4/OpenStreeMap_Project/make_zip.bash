7cc408b0ccd3719a5f9eac2cfb69206ba2cc0a86
Hansastro <jean-phi@laposte.net>
2018-09-26 12:11:23 +0200

    Add a script to generate the zip file

---
+++ Chapter4/OpenStreeMap_Project/make_zip.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip OpenStreetMap_project.zip OpenStreetMap_Project.ipynb Apensen.osm README.txt Ressources.txt OpenStreetMap_Project.pdf OpenStreetMap_Project.py data_wrangling_schema.sql
