c29f30782ea27564018617351a4fdba528dd5d6e
Hansastro <jean-phi@laposte.net>
2019-02-20 10:15:07 +0100

    Add final project

--- Chapter6/ud120-projects/final_project/poi_id.py
+++ Chapter6/ud120-projects/final_project/poi_id.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import sys
 import pickle
+import sys
 import numpy as np
-from sklearn.model_selection import StratifiedShuffleSplit
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 sys.path.append("../tools/")
 
@@ -13,15 +14,54 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary', 'bonus', 'deferred_income', 'total_stock_value', 'exercised_stock_options', 'long_term_incentive', 'total_benefit', 'fraction_to_poi' ] # You will need to use more features
+descision_tree_features_list = ['poi',
+                 'exercised_stock_options',
+                 'expenses',
+                 'fraction_to_poi',
+                 'total_benefit',
+                 'shared_receipt_with_poi'
+                 ]
+
+best_features_list = [ 'poi',
+                      'exercised_stock_options',
+                      'total_stock_value',
+                      'bonus',
+                      'salary',
+                      'total_benefit',
+                      'fraction_to_poi']
+
+features_list = descision_tree_features_list
+# features_list = ['poi',
+#                  'to_messages',
+#                  'deferral_payments',
+#                  'expenses',
+#                  'deferred_income',
+#                  'from_poi_to_this_person',
+#                  'fraction_from_poi',
+#                  'restricted_stock_deferred',
+#                  'shared_receipt_with_poi',
+#                  'loan_advances',
+#                  'from_messages',
+#                  'other',
+#                  'director_fees',
+#                  'bonus',
+#                  'total_stock_value',
+#                  'from_this_person_to_poi',
+#                  'long_term_incentive',
+#                  'restricted_stock',
+#                  'salary',
+#                  'total_payments',
+#                  'fraction_to_poi',
+#                  'exercised_stock_options',
+#                  'total_benefit']
 
 ### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "br") as data_file:
+with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-# Remove of the mail address field
-del  data_dict['TOTAL']
+del data_dict['TOTAL']
+del data_dict['THE TRAVEL AGENCY IN THE PARK']
 
 ### Task 3: Create new feature(s)
 # --> Total money (sum of all benefit total_payement + total_stock_value)
@@ -90,7 +130,38 @@ labels, features = targetFeatureSplit(data)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn import tree
-clf = tree.DecisionTreeClassifier(max_depth=7, min_samples_leaf=2, min_samples_split=4)
+#from sklearn.naive_bayes import GaussianNB
+#clf = GaussianNB()
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(splitter='best', max_leaf_nodes=5, min_samples_leaf=4, random_state=42, criterion='entropy', min_samples_split=2, max_depth=2)
+#clf = DecisionTreeClassifier()
+
+#from sklearn.neighbors import KNeighborsClassifier
+#clf = KNeighborsClassifier(n_neighbors=6, leaf_size=1, algorithm='ball_tree', p=2)
+#clf = KNeighborsClassifier()
+
+# scaler = MinMaxScaler()
+# features_rescaled = scaler.fit_transform(features)
+# params = {'n_neighbors': [2,3,4,5,6,7,8,9,10],
+#           'algorithm' : ['ball_tree', 'kd_tree', 'brute'],
+#           'leaf_size': [1, 2, 3, 4, 5, 10, 20, 30, 40, 50],
+#           'p': [1, 2, 3, 4, 5]}
+# gridSearch = GridSearchCV(KNeighborsClassifier(), params)
+
+# params = {'criterion': ['gini', 'entropy'],
+#           'splitter': ['best', 'random'],
+#           'max_depth': [None, 1, 2, 5, 10, 20],
+#           'min_samples_split': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+#           'min_samples_leaf': [1, 2, 3, 4, 5, 6, 7, 8],
+#           #'max_features': ['auto', 'sqrt', 'log2', None, 1, 3, 5, 7, 10],
+#           'random_state': [42],
+#           'max_leaf_nodes': [None, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#           }
+# gridSearch = GridSearchCV(DecisionTreeClassifier(), params)
+
+# gridSearch.fit(features, labels)
+# print(gridSearch.best_params_)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -111,10 +182,36 @@ def datasetSplit(features, labels, test_size=0.3, random_state=42):
     return features_train, features_test, labels_train, labels_test
 
 
-scaler = MinMaxScaler()
-features_selected = my_dataset
-#features_rescaled = scaler.fit_transform(features_selected)
-features_rescaled = features_selected
+def scaleDict(dict_):
+    '''
+    Scale a dictionary, replace the NaN value by 0 and remove the email_address column if present
+
+    parameter:
+    - the dictionary of the dataset
+
+    return:
+    - the scaled dictionary
+    '''
+    # Convert the dictionay in a dataframe
+    dataset = pd.DataFrame.from_dict(dict_, orient='index')
+
+    # if the column email_address exists remove it (it is just text redondant with the name)
+    if 'email_address' in dataset.columns:
+        dataset.drop('email_address', axis=1, inplace=True)
+    # Replace the NaN value by 0. Becareful in the pickel NaN is a string and not a type.
+    dataset.replace('NaN', 0, inplace=True)
+
+    # Rescale the data
+    scaler = MinMaxScaler()
+    features_rescaled = scaler.fit_transform(dataset)
+
+    # recreate the dictionary and return it
+    return pd.DataFrame(features_rescaled, index=dataset.index, columns=dataset.columns).to_dict(orient='index')
+
+    data = featureFormat(dataset, feature_list, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+
+features_rescaled = my_dataset
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
